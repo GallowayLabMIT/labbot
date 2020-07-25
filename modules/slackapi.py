82fcd4ded513faf1f4b2dd784b2cfd118da3543b
Christopher Johnstone <meson800@gmail.com>
2020-07-25 10:07:15 -0700

    Added validiation code to arrival/departure time

--- modules/slackapi.py
+++ modules/slackapi.py
@@ -4,7 +4,10 @@ import uvicorn
 from fastapi import Body, FastAPI, Request
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional, List
+import json
 import pytz
+import re
 
 ETC = pytz.timezone('America/New_York')
 
@@ -19,7 +22,7 @@ class UserModel(BaseModel):
     username: str
     team_id: str
 
-class InteractionPayload(BaseModel):
+class ShortcutPayload(BaseModel):
     type: str
     token: str
     action_ts: str
@@ -27,6 +30,54 @@ class InteractionPayload(BaseModel):
     user: UserModel
     callback_id: str
     trigger_id: str
+
+class ContainerModel(BaseModel):
+    type: str
+    view_id: str
+
+class ViewModel(BaseModel):
+    id: str
+    team_id: str
+    type: str
+    blocks: list
+    private_metadata: Optional[str] = ''
+    callback_id: str
+    state: dict
+    hash: str
+    title: dict
+    clear_on_close: bool
+    notify_on_close: bool
+    close: Optional[dict] = None
+    submit: Optional[dict] = None
+
+class ActionModel(BaseModel):
+    action_id: str
+    block_id: str
+    text: dict
+    value: str
+    type: str
+    action_ts: str
+
+class BlockActionPayload(BaseModel):
+    type: str
+    user: UserModel
+    api_app_id: str
+    token: str
+    container: ContainerModel
+    trigger_id: str
+    team: TeamModel
+    view: ViewModel
+    actions: List[ActionModel]
+
+class ViewSubmissionPayload(BaseModel):
+    type: str
+    team: TeamModel
+    user: UserModel
+    api_app_id: str
+    token: str
+    trigger_id: str
+    view: ViewModel
+    response_urls: list
 
 slackapi_app = FastAPI()
 sclient = None
@@ -37,63 +88,87 @@ main_model = {
     "type": "plain_text",
     "text": "COVID hour tracking"
   },
-  "blocks": [
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Submit your arrival and depature times"
-      },
-      "block_id": "section1",
-    },
-    {
-      "type": "input",
-      "label": {
-        "type": "plain_text",
-        "text": "Date"
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Submit your lab arrival and depature times:"
+            },
+            "block_id": "header_section"
         },
-      "element": {
-        "type": "datepicker",
-        "action_id": "submit_date",
-        "initial_date": "2020-01-01"
+        {
+            "type": "divider"
         },
-      "optional": False
-    },
-    {
-      "type": "input",
-      "label": {
-        "type": "plain_text",
-        "text": "Arrival time"
-      },
-      "element": {
-        "type": "plain_text_input",
-        "action_id": "arrival_time",
-        "placeholder": {
-          "type": "plain_text",
-          "text": "Use 24-hour time (HH:MM)"
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "This week you have used *15* out of *25* hours.\nAll lab members have used *15* out of *150* hours."
+            },
+            "accessory": {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "View your submissions"
+                },
+                "value": "view_submissions_button"
+            }
         },
-        "multiline": False
-      },
-      "optional": False
-    },
-    {
-      "type": "input",
-      "label": {
-        "type": "plain_text",
-        "text": "Depature time"
-      },
-      "element": {
-        "type": "plain_text_input",
-        "action_id": "depature_time",
-        "placeholder": {
-          "type": "plain_text",
-          "text": "Use 24-hour time (HH:MM)"
+        {
+            "type": "divider"
         },
-        "multiline": False
-      },
-      "optional": False
-    }
-  ],
+        {
+            "type": "input",
+            "label": {
+                "type": "plain_text",
+                "text": "Date"
+            },
+            "element": {
+                "type": "datepicker",
+                "action_id": "submit_date",
+                "initial_date": "2020-01-01"
+            },
+            "optional": False,
+            "block_id": "submit_date_input"
+        },
+        {
+            "type": "input",
+            "label": {
+                "type": "plain_text",
+                "text": "Arrival time"
+            },
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "arrival_time",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Use 24-hour time (H:MM or HH:MM)"
+                },
+                "multiline": False
+            },
+            "optional": False,
+            "block_id": "arrival_time_input"
+        },
+        {
+            "type": "input",
+            "label": {
+                "type": "plain_text",
+                "text": "Departure time"
+            },
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "departure_time",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Use 24-hour time (H:MM or HH:MM)"
+                },
+                "multiline": False
+            },
+            "optional": False,
+            "block_id": "departure_time_input"
+        }
+    ],
   "close": {
     "type": "plain_text",
     "text": "Cancel"
@@ -103,22 +178,88 @@ main_model = {
     "text": "Submit hours"
   },
   "private_metadata": "Shhhhhhhh",
-  "callback_id": "view_identifier_12"
+  "callback_id": "covid_hour_track_submission"
+}
+
+submissions_model = {
+	"type": "modal",
+	"title": {
+		"type": "plain_text",
+		"text": "Submitted hours"
+	},
+	"close": {
+		"type": "plain_text",
+		"text": "Close"
+	},
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "For the week of *July 15* to *July 27*, you have submitted *25.4* hours:"
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*7/25*: 9:30 to 11:30"
+			}
+		}
+	]
 }
 
 @slackapi_app.post("/slack/covid_hours")
 async def root(request: Request):
     form_data = await request.form()
-    print(form_data)
-    payload = InteractionPayload.parse_raw(form_data['payload'])
+    print(json.loads(form_data['payload']))
+    payload_raw = json.loads(form_data['payload'])
+    if payload_raw['type'] == 'shortcut':
+        payload = ShortcutPayload.parse_raw(form_data['payload'])
 
-    now_dt = datetime.now(ETC)
-    main_model["blocks"][1]["element"]["initial_date"] = now_dt.strftime('%Y-%m-%d')
-    sclient.views_open(
-        trigger_id = payload.trigger_id,
-        view = main_model)
-    
-    return {"message": "Hello World"}
+        # Update the main model
+        now_dt = datetime.now(ETC)
+        main_model["blocks"][4]["element"]["initial_date"] = now_dt.strftime('%Y-%m-%d')
+        sclient.views_open(
+            trigger_id = payload.trigger_id,
+            view = main_model)
+    elif payload_raw['type'] == 'block_actions':
+        payload = BlockActionPayload.parse_raw(form_data['payload'])
+        print(payload.actions)
+    elif payload_raw['type'] == 'view_submission':
+        # We received a submission on the covid hour tracking modal.
+        payload = ViewSubmissionPayload.parse_raw(form_data['payload'])
+        form_values = payload.view.state['values']
+
+        # Perform validiation on  the times
+        time_regex = '^([01]?[0-9]|2[0-3]):([0-5][0-9])$'
+        arrival = re.match(time_regex, form_values['arrival_time_input']['arrival_time']['value'])
+        departure = re.match(time_regex, form_values['departure_time_input']['departure_time']['value'])
+
+        errors = {}
+        if arrival is None:
+            errors['arrival_time_input'] = 'You must enter arrival time in the 24-hour format (H:MM or HH:MM)'
+        if departure is None:
+            errors['departure_time_input'] = 'You must enter departure time in the 24-hour format (H:MM or HH:MM)'
+
+        if arrival is not None and departure is not None:
+            arrival_time = int(arrival.group(1)) + float(arrival.group(2)) / 60
+            departure_time = int(departure.group(1)) + float(departure.group(2)) / 60
+
+            if departure_time < arrival_time:
+                error_msg = 'Departure time must be later than arrival time! '
+                if departure_time < 4:
+                    error_msg += 'To submit hours crossing midnight, please submit as two'
+                    error_msg += ' separate time intervals for each day'
+                errors['departure_time_input'] = error_msg
+        if len(errors) > 0:
+            return {
+                "response_action": "errors",
+                "errors": errors}
+    return
 
 proc = None
 
