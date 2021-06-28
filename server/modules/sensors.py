09449404fb2d3bf4ceef6a5816319b18160b79f2
Christopher Johnstone <meson800@gmail.com>
2021-06-28 15:11:49 -0400

    Updated sensors module to write temp measurements into a SQlite database

--- server/modules/sensors.py
+++ server/modules/sensors.py
@@ -2,10 +2,14 @@
 Module that tracks various in-lab sensors using MQTT and iMonnit.
 """
 from labbot.module_loader import ModuleLoader
+import fastapi 
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import paho.mqtt.client as mqtt
 from pydantic import BaseModel
 import typing
-import json
+import sqlite3
+import secrets
+import datetime
 
 
 class MonnitGatewayMessage(BaseModel):
@@ -47,6 +51,34 @@ module_config = {}
 
 loader = ModuleLoader()
 
+mqtt_client = mqtt.Client()
+
+# Initalize database connection
+db_con = sqlite3.connect('sensors.db')
+cursor = db_con.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS sensors (
+    id integer PRIMARY KEY,
+    type integer NOT NULL,
+    name text NOT NULL
+);
+CREATE TABLE IF NOT EXISTS temperature_measurements (
+    datetime text,
+    sensor integer NOT NULL,
+    measurement real NOT NULL,
+    FOREIGN KEY (sensor)
+        REFERENCES sensors (sensor)
+);
+CREATE TABLE IF NOT EXISTS alarm_measurements (
+    datetime text,
+    sensor integer NOT NULL,
+    measurement real NOT NULL,
+    FOREIGN KEY (sensor)
+        REFERENCES sensors (sensor)
+);
+''')
+
+
 def register_module(config):
     # Override defaults if present 
     module_config.update(config)
@@ -60,9 +92,53 @@ def register_module(config):
         raise RuntimeError("Expected to be passed client credentials in keys 'username' and 'password' under 'mqtt'!")
     if 'url' not in module_config['mqtt'] or 'port' not in module_config['mqtt']:
         raise RuntimeError("Expected to be passed broker location ('url' and 'port') in key 'mqtt'!")
+    if 'client_id' not in module_config['mqtt']:
+        raise RuntimeError("Expected to be passed a persistant client ID ('client_id') in key 'mqtt'!")
+    
+    
+    # Attempt to connect to MQTT
+    mqtt_client.reinitalize(client_id=module_config['mqtt']['client_id'], clean_session=False)
+    mqtt_client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+    mqtt_client.on_connect = lambda x: x
+    mqtt_client.on_message = lambda x: x
+    mqtt_client.username_pw_set(module_config['mqtt']['username'], module_config['mqtt']['password'])
+    mqtt_client.connect_async(module_config['mqtt']['url'], module_config['mqtt']['port'], keepalive=60)
+
     return loader
 
+imonnit_security = HTTPBasic()
+
 @loader.fastapi.post("/imonnit_endpoint")
-def imonnit_push(message: MonnitMessage):
+def imonnit_push(message: MonnitMessage, credentials: HTTPBasicCredentials = fastapi.Depends(imonnit_security)):
+    if not (
+        secrets.compare_digest(credentials.username, module_config['iMonnit_webhook']['username']) and
+        secrets.compare_digest(credentials.password, module_config['iMonnit_webhook']['password'])):
+        raise fastapi.HTTPException(
+            status_code = fastapi.status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"}
+        )
+    
+    for s_message in message.sensorMessages:
+        # See if sensor is already in database. If not, add it as a 
+        cursor.execute("SELECT id FROM sensors WHERE type=0 AND name=?;", s_message.sensorName)
+        if not cursor.fetchone():
+            cursor.execute("INSERT INTO sensors(type,name) VALUES (?,?);", 0, s_message.sensorName)
+        # Find sensor ID, writing measurement into 
+        cursor.execute("SELECT id FROM sensors WHERE type=0 AND name=?;", s_message.sensorName)
+        sensor_id = cursor.fetchone()
+        cursor.execute(
+            "INSERT INTO temperature_measurements(datetime,sensor,measurement) VALUES (?,?,?)",
+            datetime.datetime.now(datetime.timezone.utc).isoformat(),
+            sensor_id,
+            float(s_message.dataValue)
+        )
+
     module_config['logger'](f'Got message {message}')
     return {'success': True}
+
+@loader.timer
+def poll_mqtt(slack_client):
+    # Call the MQTT loop command once every ten seconds
+    mqtt_client.loop(timeout=1.0)
+    return 10
