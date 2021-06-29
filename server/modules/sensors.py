8c87db806e0fd1ac733a22cb4cc6db687ba5c520
Christopher Johnstone <meson800@gmail.com>
2021-06-29 17:34:35 -0400

    Added reconnect to MQTT client

--- server/modules/sensors.py
+++ server/modules/sensors.py
@@ -151,6 +151,9 @@ def imonnit_push(message: MonnitMessage, credentials: HTTPBasicCredentials = fas
 
 @loader.timer
 def poll_mqtt(slack_client):
+    # Check that MQTT is still alive
+    if not mqtt_client.connected:
+        mqtt_client.reconnect()
     # Call the MQTT loop command once every ten seconds
     mqtt_client.loop(timeout=1.0)
     rand_state = random.randint(0,2)
